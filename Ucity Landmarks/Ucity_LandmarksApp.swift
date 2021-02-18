00f5ee4a3788af4466cec63346b24159673ed327
Denzel Abernathy-Suttle <denzelthedeveloper@icloud.com>
2021-02-17 23:58:59 -0500

    First commit to new project

---
+++ Ucity Landmarks/Ucity_LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ucity_LandmarksApp.swift
+//  Ucity Landmarks
+//
+//  Created by Denzel Abernathy-Suttle on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Ucity_LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
