f04af1b4f2f5589cfbaa03290566fa31d72516df
Denzel Abernathy-Suttle <denzelthedeveloper@icloud.com>
2021-02-20 22:40:38 -0500

    Created custom image view via imported file in assests

---
+++ Ucity Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Ucity Landmarks
+//
+//  Created by Denzel Abernathy-Suttle on 2/20/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("RabePark")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
