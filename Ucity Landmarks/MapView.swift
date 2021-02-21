0cfb9a6ad4058ae08fdea27bfb2e7c928e10ccf2
Denzel Abernathy-Suttle <denzelthedeveloper@icloud.com>
2021-02-21 14:50:42 -0500

    Imported new framework for MapView, general coordinates added

---
+++ Ucity Landmarks/MapView.swift
@@ -0,0 +1,26 @@
+//
+//  MapView.swift
+//  Ucity Landmarks
+//
+//  Created by Denzel Abernathy-Suttle on 2/20/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 38.663901, longitude: -90.328165),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
