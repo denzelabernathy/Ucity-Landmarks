ed3e7ec90846eb924183f682ec3b0c19e0260eb2
Denzel Abernathy-Suttle <denzelthedeveloper@icloud.com>
2021-03-17 00:47:59 -0400

    left off on section two of lists and navigation. type bug and error still makes build fail

--- Ucity Landmarks/Model/ModelData.swift
+++ Ucity Landmarks/Model/ModelData.swift
@@ -1,9 +1,9 @@
-//
-//  ModelData.swift
-//  Ucity Landmarks
-//
-//  Created by Denzel Abernathy-Suttle on 2/25/21.
-//
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Storage for model data.
+*/
 
 import Foundation
 
@@ -13,8 +13,8 @@ func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
 
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle.")
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
     }
 
     do {
