edb7367c76fb591f54bf3692cf6daf62a97386a4
Denzel Abernathy-Suttle <denzelthedeveloper@icloud.com>
2021-04-14 14:14:37 -0400

    finished off section two with adding preview devices to see how the code renders on different sized devices

--- Ucity Landmarks/Views/LandmarkList.swift
+++ Ucity Landmarks/Views/LandmarkList.swift
@@ -22,6 +22,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone 11", "iPhone XS Max", "iPad (8th generation)"], id:\.self){ deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
