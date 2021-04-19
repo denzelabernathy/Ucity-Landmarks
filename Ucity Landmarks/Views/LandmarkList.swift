5b6022a736f3175a26cd2c3b9e134c058126fb08
Denzel Abernathy-Suttle <denzelthedeveloper@icloud.com>
2021-04-19 15:08:01 -0400

    added toggle with the binding of ison based on the value of the state structure showfavorites

--- Ucity Landmarks/Views/LandmarkList.swift
+++ Ucity Landmarks/Views/LandmarkList.swift
@@ -8,30 +8,38 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State private var showFavoritesOnly = true
+    @State private var showFavoritesOnly = false
     
     var filteredLandmarks: [Landkmark] {
-        landmarks.filter { landmark in (!showFavoritesOnly || landmark.isFavorite)}
+        landmarks.filter { landmark in (!showFavoritesOnly || landmark.isFavorite)
+        }
     }
+    
     
     var body: some View {
         NavigationView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Your favs")
                 }
-                .navigationTitle("Landmarks")
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                    .navigationTitle("Landmarks")
+                }
             }
         }
     }
-}
-
-struct LandmarkList_Previews: PreviewProvider {
-    static var previews: some View {
-//        ForEach(["iPhone 11", "iPhone XS Max", "iPad (8th generation)"], id:\.self){ deviceName in
+    
+    struct LandmarkList_Previews: PreviewProvider {
+        static var previews: some View {
+            //        ForEach(["iPhone 11", "iPhone XS Max", "iPad (8th generation)"], id:\.self){ deviceName in
             LandmarkList()
-//                .previewDevice(PreviewDevice(rawValue: deviceName))
-//                .previewDisplayName(deviceName)
+            //                .previewDevice(PreviewDevice(rawValue: deviceName))
+            //                .previewDisplayName(deviceName)
         }
     }
-//}
+    //}
+}
