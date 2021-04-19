50485d9597bcba0ad35a4e10ec6aa343e3694360
Denzel Abernathy-Suttle <denzelthedeveloper@icloud.com>
2021-04-19 10:28:27 -0400

    used the state structure to filter the view of the listed items and passed it with the logical operator

--- Ucity Landmarks/Views/LandmarkList.swift
+++ Ucity Landmarks/Views/LandmarkList.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = true
+    
+    var filteredLandmarks: [Landkmark] {
+        landmarks.filter { landmark in (!showFavoritesOnly || landmark.isFavorite)}
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                     LandmarkRow(landmark: landmark)
                 }
@@ -22,10 +28,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone 11", "iPhone XS Max", "iPad (8th generation)"], id:\.self){ deviceName in
+//        ForEach(["iPhone 11", "iPhone XS Max", "iPad (8th generation)"], id:\.self){ deviceName in
             LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
         }
     }
-}
+//}
